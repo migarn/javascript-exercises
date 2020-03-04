fad17b0d23f69f766ffeed0fd2b3d8e3ffe63e3b
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-04 18:32:03 +0100

    indices var added

--- JS-21-Nawigacja/navbar.js
+++ JS-21-Nawigacja/navbar.js
@@ -7,6 +7,7 @@ var navURLs  = new Array('astronomy.html', 'science.html', 'sports.html', 'music
 var linkText = new Array('Astronomy', 'Other Sciences', 'Sports', 'Musicians\' Corner', 'Cool People');
 var currentDate = new Date();
 var summedTime = currentDate.getFullYear() + currentDate.getMonth() + 1 + currentDate.getDate() + currentDate.getHours() + currentDate.getMinutes() + currentDate.getSeconds();
+var indices = new Array(navURLs.length);
 
 function navbar() {
 	var navStr= '';
