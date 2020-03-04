f9a8d4458be560ca0b5032a7afd833e514de826d
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-04 18:21:05 +0100

    summedTime var added

--- JS-21-Nawigacja/navbar.js
+++ JS-21-Nawigacja/navbar.js
@@ -5,13 +5,15 @@
 
 var navURLs  = new Array('astronomy.html', 'science.html', 'sports.html', 'music.htm', 'people.htm');
 var linkText = new Array('Astronomy', 'Other Sciences', 'Sports', 'Musicians\' Corner', 'Cool People');
+var currentDate = new Date();
+var summedTime = currentDate.getFullYear() + currentDate.getMonth() + 1 + currentDate.getDate() + currentDate.getHours() + currentDate.getMinutes() + currentDate.getSeconds();
 
 function navbar() {
 	var navStr= '';
 	for (var i = 0; i < navURLs.length; i++) {
 		//	if (location.href != navURLs[i]) {		    	   Uncomment this line and comment 
 		if (location.href.indexOf(navURLs[i]) == -1) {	// this one when not browsing locally
-			navStr += ' <B>[</B><A HREF="' + navURLs[i] + '">' + linkText[i] + '</A><B>]</B> '; 
+			navStr += ' <B>[</B><A HREF="' + navURLs[i] + '">' + linkText[i] + '</A><B>]</B> ';
 			}
 		}
 	document.writeln('<BR><BR>' + navStr);
