082caa68bcd88ade99469f1926545a0e61c65732
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-04 18:40:48 +0100

    Loop for completing indices added

--- JS-21-Nawigacja/navbar.js
+++ JS-21-Nawigacja/navbar.js
@@ -5,11 +5,21 @@
 
 var navURLs  = new Array('astronomy.html', 'science.html', 'sports.html', 'music.htm', 'people.htm');
 var linkText = new Array('Astronomy', 'Other Sciences', 'Sports', 'Musicians\' Corner', 'Cool People');
-var currentDate = new Date();
-var summedTime = currentDate.getFullYear() + currentDate.getMonth() + 1 + currentDate.getDate() + currentDate.getHours() + currentDate.getMinutes() + currentDate.getSeconds();
-var indices = new Array(navURLs.length);
 
 function navbar() {
+	var currentDate = new Date();
+	var summedTime = currentDate.getFullYear() + currentDate.getMonth() + 1 + currentDate.getDate() + currentDate.getHours() + currentDate.getMinutes() + currentDate.getSeconds();
+	var indices = new Array(navURLs.length);
+	var currentIndex = summedTime % navURLs.length;
+	
+	for (var i = 0; i < indices.length; i++) {
+		indices[i] = currentIndex;
+		currentIndex++;
+		if (currentIndex == navURLs.length) {
+			currentIndex = 0;
+		}
+	}
+	
 	var navStr= '';
 	for (var i = 0; i < navURLs.length; i++) {
 		//	if (location.href != navURLs[i]) {		    	   Uncomment this line and comment 
