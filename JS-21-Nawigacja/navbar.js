3fd41af8c85d10c5af912b0301f89e2d90298f34
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-03-07 11:49:44 +0100

    Description in JS-21 removed

--- JS-21-Nawigacja/navbar.js
+++ JS-21-Nawigacja/navbar.js
@@ -1,20 +1,5 @@
 // navbar.js
 
-/*
-Modyfikacja:
-Zmieniaj kolejność elementów w zależności od aktualnego czasu
-
-Przyjąłem, że aktualny czas będzie reprezentowany przez liczbę powstałą przez zsumowanie aktualnych: roku
-miesiąca, dnia, godziny, minuty i sekundy. Następnie dla liczby tej obliczam resztę z dzielenia przez
-długość tablicy zawierającej adresy URL. Reszta ta zawsze będzie równa jednemu z indeksów tablicy (dla
-rozważanego przykładu obliczam resztę z dzielenia przez 5, co daje mi 0, 1, 2, 3 lub 4) i tę wartość przyjmuję
-jako pierwszy element nowej tablicy 'indices'. Tablicę tę uzupełniam kolejnymi wartościami indeksów (w odpowiednim
-momencie przechodząc do wartości 0). Następnie w pętli uzupełniającej wartość zmiennej 'navStr' jako indeksy
-tablic 'navURLs' oraz 'linkText' nie przyjmuję kolejnych wartości zmiennej 'i' lecz kolejne wartości tablicy
-'indices'.
-
-*/
-
 var navURLs  = new Array('astronomy.html', 'science.html', 'sports.html', 'music.htm', 'people.htm');
 var linkText = new Array('Astronomy', 'Other Sciences', 'Sports', 'Musicians\' Corner', 'Cool People');
 
